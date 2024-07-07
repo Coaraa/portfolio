b1bf8d564039ecdb9d2c77e5ff42e2a79657fd24
Coaraa <clara.sithidej@gmail.com>
2024-07-07 18:08:00 +0200

    project and about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,21 +1,22 @@
-import React from 'react'
+import React from 'react';
+import '../App.css';
+import '../styles/AboutMe.css'; 
 
 function AboutMe() {
   return (
-    <div>
-      <div className='titre-section'>À propos de moi</div>
-      <div className='container'>
-        <div className='portrait'>
-          <img src="" alt="Portrait" />
-        </div>
-        <div className='description'>
-          <div className='texte'>
-            <p>Je m'appelle Clara, j'ai 20 ans et je suis actuellement étudiante ingénieure en informatique à Polytech Lyon.</p>
-            <p>Au cours de mon cursus, j'ai étudié les mathématiques, le développement d'applications, le développement web et la data science. J'ai également appris la gestion de projet, la communication et le travail en équipe.</p>
-            <p>J'aime construire des applications belles et efficaces en combinant mes connaissances techniques avec le design. Mon objectif est de produire des projets à la fois intuitifs et évolutifs.</p>
+    <div className='section about-container'>
+      <div className='portrait'>
+        <img src="" alt="Portrait" />
+      </div>
+      <div className='description'>
+        <div className='title-section'>À propos de moi</div>
 
-          </div>
+        <div className='texte'>
+          <p>Je m'appelle Clara, j'ai 20 ans et je suis actuellement étudiante ingénieure en informatique à Polytech Lyon.</p>
+          <p>Au cours de mon cursus, j'ai étudié les mathématiques, le développement d'applications, le développement web et la data science. J'ai également appris la gestion de projet, la communication et le travail en équipe.</p>
+          <p>J'aime construire des applications belles et efficaces en combinant mes connaissances techniques avec le design. Mon objectif est de produire des projets à la fois intuitifs et évolutifs.</p>
         </div>
+
       </div>
     </div>
   )
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,9 +1,35 @@
 import React from 'react'
+import '../App.css'
+import '../styles/Projects.css'
 
 function Projects() {
   return (
-    <div>
-      <p>Mes projets</p>
+    <div className="section">
+      <div className="title-section">Mes projets</div>
+
+      <div className="projects">
+        <div className="project-card">
+          <div className="project-title">Carte interactive de la couverture réseau française</div>
+          <div className="project-year">2023</div>
+          <div className="project-description">Ce projet vise à fournir une visualisation interactive de la couverture réseau en France sous forme de carte et de tableau. Cette application est destinée aussi bien aux professionnels, qui peuvent déterminer les lieux où l’accès au réseau est le plus restreint, ainsi qu’aux particuliers qui peuvent l’utiliser afin de savoir quel réseau est disponible dans leur logement.</div>
+          <div className="project-technologies">Technologies : HTML, CSS, JavaScript, React, Node.js, SQL, Leaflet</div>
+          <div className="project-tools">Outils : Visual Studio Code, Xampp</div>
+          <div className="project-git-link">
+            <a href="https://github.com/Coaraa/telecom" title="Lien GitHub">Lien GitHub</a>
+          </div>
+        </div>
+
+        <div className="project-card">
+          <div className="project-title">Site fictif de boutique en ligne</div>
+          <div className="project-year">2023</div>
+          <div className="project-description">Ce site fictif de boutique en ligne propose une sélection de produits de différentes catégories. Vous pourrez réaliser l'ensemble des actions d'un site e-commerce classique : consulter les produits, les ajouter au panier, vous connecter, passer commande, etc. Il existe également une interface d'administration pour gérer les produits et les commandes.</div>
+          <div className="project-technologies">Technologies : HTML, CSS, JavaScript, PHP, MySQL, Twig, Bootstrap 5</div>
+          <div className="project-tools">Outils : Visual Studio Code, Xampp</div>
+          <div className="project-git-link">
+            <a href="https://github.com/Coaraa/shopping-bag" title="Lien GitHub">Lien GitHub</a>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
