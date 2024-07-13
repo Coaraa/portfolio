eb067e52449e051bbdf5c08751ffa2744d6c20e6
Coaraa <clara.sithidej@gmail.com>
2024-07-13 18:55:42 +0200

    responsive design

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -29,6 +29,40 @@ function Projects() {
             <a href="https://github.com/Coaraa/shopping-bag" title="GitHub" target="_blank" rel="noopener noreferrer">Lien GitHub du projet</a>
           </div>
         </div>
+
+        <div className="project-card">
+          <div className='project-title'>It takes IO</div>
+          <div className='project-year'>2022</div>
+          <div className='project-description'>It takes IO est un jeu de plateforme à deux joueurs. Ceux-ci doivent évoluer à travers les différents niveaux pour ouvrir la porte principale et accéder au niveau suivant. Ce jeu est basé sur un ensemble d'énigmes à résoudre pour activer des portes logiques. Les deux personnages incarnés sont ainsi complémentaires en représentant un 0 et un 1. Le but est ainsi de coopérer pour terminer tous les niveaux en trouvant la bonne combinaison pour déverrouiller la porte et en évitant les obstacles.</div>
+          <div className='project-technologies'>C++ / SFML</div>
+          <div className='project-tools'>Visual Studio Code / Linux</div>
+          <div className='project-git-link'>
+            <a href='https://github.com/Coaraa/It-takes-IO' title='GitHub' target='_blank' rel='noopener noreferrer'>Lien GitHub du projet</a>
+          </div>
+        </div>
+
+        <div className="project-card">
+          <div className="project-title">SokIOban</div>
+          <div className="project-year">2024</div>
+          <div className="project-description">SokIOban est un jeu en deux dimensions où le joueur doit placer des caisses sur des cases cibles. Il peut se déplacer dans les quatre directions et pousser une seule caisse à la fois. Une fois toutes les caisses sur les boutons, le niveau est réussi et le joueur passe au niveau suivant. L'objectif est de réussir avec le moins de coups possibles.</div>
+          <div className="project-technologies">Java / Swing</div>
+          <div className="project-tools">IntelliJ IDEA</div>
+          <div className="project-git-link">
+            <a href="https://github.com/Coaraa/Sokoban" title="GitHub" target="_blank" rel="noopener noreferrer">Lien GitHub du projet</a>
+          </div>
+        </div>
+
+        <div className="project-card">
+          <div className="project-title">Chat de discussion UDP</div>
+          <div className="project-year">2024</div>
+          <div className="project-description">Ce projet est un chat de discussion local utilisant le protocole UDP. Il permet à plusieurs clients connectés au même réseau de discuter en temps réel. Les messages envoyés par un client sont reçus par les clients dans la même salle de discussion. Chaque client dispose de son propre pseudo et peut utiliser différentes commandes pour interagir avec le chat.</div>
+          <div className="project-technologies">Java / DatagramSocket</div>
+          <div className="project-tools">IntelliJ IDEA</div>
+          <div className="project-git-link">
+            <a href="https://github.com/Coaraa/ChatUDP" title="GitHub" target="_blank" rel="noopener noreferrer">Lien GitHub du projet</a>
+          </div>
+        </div>
+
       </div>
     </div>
   )
