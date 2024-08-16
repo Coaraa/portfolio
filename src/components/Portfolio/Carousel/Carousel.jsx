087b208f75d517c19433c2aab363288933131062
Coaraa <clara.sithidej@gmail.com>
2024-08-16 19:27:51 +0200

    add more projects

--- src/components/Portfolio/Carousel/Carousel.jsx
+++ src/components/Portfolio/Carousel/Carousel.jsx
@@ -7,20 +7,20 @@ import "slick-carousel/slick/slick-theme.css";
 const data = [
   {
     id: 1,
-    title: 'Carte interactive de la couverture réseau française',
-    description: 'Ce projet propose une visualisation interactive de la couverture réseau en France sous forme de carte et de tableau.',
-    image: '/carousel/telecom.png',
-    year: '2023',
-    technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'SQL', 'Leaflet'],
-    tools: ['Visual Studio Code', 'Xampp'],
-    gitLink: <a href="https://github.com/Coaraa/telecom" title="GitHub" target="_blank" rel="noopener noreferrer">GitHub</a>
-  },
+    title: 'Slot machine',
+    description: "Ce projet est une machine à sous permettant de jouer avec de l'argent fictif. Il est possible de miser, de lancer la machine et de gagner des crédits.",
+    image: "/carousel/slot-machine.png",
+    year: 'Août 2024',
+    technologies: ['HTML', 'CSS', 'JavaScript'],
+    tools: ['Visual Studio Code', 'Adobe Illustrator'],
+    gitLink: <a href="https://github.com/Coaraa/Slot-machine" title="GitHub" target="_blank" rel="noopener noreferrer">GitHub</a>
+  }, 
   {
     id: 2,
     title: 'Site fictif de boutique en ligne',
     description: "Ce E-commerce fictif vous permet de réaliser l'ensemble des actions classiques : commander des produits ou vous connecter. Il fournit une interface d'administration simple.",
     image: "/carousel/boutique.png",
-    year: '2023',
+    year: 'Janvier 2024',
     technologies: ['HTML', 'CSS', 'JavaScript', 'PHP', 'Twig', 'MySQL', 'Bootstrap'],
     tools: ['Visual Studio Code', 'Xampp'],
     gitLink: <a href="https://github.com/Coaraa/shopping-bag" title="GitHub" target="_blank" rel="noopener noreferrer">GitHub</a>
@@ -30,30 +30,50 @@ const data = [
     title: 'It takes IO',
     description: "It takes IO est un jeu de plateforme local à deux joueurs basé sur un ensemble d'énigmes à résoudre pour activer des portes logiques.",
     image: "/carousel/it-takes-io.png",
-    year: '2024',
+    year: 'Mars 2023',
     technologies: ['C++', 'SFML'],
     tools: ['Visual Studio Code', 'Linux'],
     gitLink: <a href='https://github.com/Coaraa/It-takes-IO' title='GitHub' target='_blank' rel='noopener noreferrer'>GitHub</a>
   },
   {
     id: 4,
-    title: 'SokIOban',
-    description: "SokIOban est un jeu en deux dimensions où le joueur doit se déplacer dans les quatre directions afin placer des caisses sur des cases cibles.",
-    image: "/carousel/sokioban.png",
-    year: '2024',
-    technologies: ['Java', 'Swing'],
-    tools: ['IntelliJ IDEA'],
-    gitLink: <a href="https://github.com/Coaraa/Sokoban" title="GitHub" target="_blank" rel="noopener noreferrer">GitHub</a>
+    title: 'Carte interactive de la couverture réseau française',
+    description: 'Ce projet propose une visualisation interactive de la couverture réseau en France sous forme de carte et de tableau.',
+    image: '/carousel/telecom.png',
+    year: 'Septembre 2023',
+    technologies: ['HTML', 'CSS', 'JavaScript', 'React', 'Node.js', 'SQL', 'Leaflet'],
+    tools: ['Visual Studio Code', 'Xampp'],
+    gitLink: <a href="https://github.com/Coaraa/telecom" title="GitHub" target="_blank" rel="noopener noreferrer">GitHub</a>
   },
   {
     id: 5,
     title: 'Chat de discussion UDP',
     description: "Ce chat de discussion local utilise le protocole UDP pour permettre à des clients connectés au même réseau de discuter en temps réel.",
     image: "/carousel/chat-udp.png",
-    year: '2024',
+    year: 'Mai 2024',
     technologies: ['Java', 'DatagramSocket'],
     tools: ['IntelliJ IDEA'],
     gitLink: <a href="https://github.com/Coaraa/ChatUDP" title="GitHub" target="_blank" rel="noopener noreferrer">GitHub</a>
+  },
+  {
+    id: 6,
+    title: 'SokIOban',
+    description: "SokIOban est un jeu en deux dimensions où le joueur doit se déplacer dans les quatre directions afin placer des caisses sur des cases cibles.",
+    image: "/carousel/sokioban.png",
+    year: 'Mai 2024',
+    technologies: ['Java', 'Swing'],
+    tools: ['IntelliJ IDEA'],
+    gitLink: <a href="https://github.com/Coaraa/Sokoban" title="GitHub" target="_blank" rel="noopener noreferrer">GitHub</a>
+  },
+  {
+    id: 7,
+    title: 'Portfolio',
+    description: "Ce portfolio est un site web personnel permettant de présenter mes projets et mes compétences. Il est conçu avec React et Sass.",
+    image: "/carousel/portfolio.png",
+    year: 'Août 2024',
+    technologies: ['HTML', 'CSS', 'Sass', 'JavaScript', 'Node.js', 'React'],
+    tools: ['Visual Studio Code', 'Adobe Photoshop'],
+    gitLink: <a href="https://github.com/Coaraa/portfolio" title="GitHub" target="_blank" rel="noopener noreferrer">GitHub</a>
   }
 ];
 
@@ -96,7 +116,9 @@ function Carousel() {
                   tool === 'Visual Studio Code' ? <img src='/carousel/tools/vscode.png' alt='Visual Studio Code' title='Visual Studio Code' /> :
                     tool === 'IntelliJ IDEA' ? <img src='/carousel/tools/intellij.png' alt='IntelliJ IDEA' title='IntelliJ IDEA' /> :
                       tool === 'Xampp' ? <img src='/carousel/tools/xampp.png' alt='Xampp' title='Xampp' /> :
-                        tool === 'Linux' ? <img src='/carousel/tools/linux.png' alt='Linux' title='Linux' /> : null
+                        tool === 'Linux' ? <img src='/carousel/tools/linux.png' alt='Linux' title='Linux' /> : 
+                          tool === 'Adobe Illustrator' ? <img src='/carousel/tools/illustrator.png' alt='Adobe Illustrator' title='Adobe Illustrator' /> :
+                            tool === 'Adobe Photoshop' ? <img src='/carousel/tools/photoshop.png' alt='Adobe Photoshop' title='Adobe Photoshop' /> : null
                 ))}</p>
               <p className='link'>{slide.gitLink}</p>
             </div>
