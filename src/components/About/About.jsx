89728006e474b8ea6775ab91714a2eaed408fce3
Coaraa <clara.sithidej@gmail.com>
2025-01-07 19:33:20 +0100

    hero and about section

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -19,21 +19,32 @@ const variants = {
 
 const About = () => {
   return (
-    <div className="about" variants={variants} initial="initial" whileInView="animate">
-      <div className='avatarContainer'>
-        <img src="/avatar.png" alt="Avatar" />
+    <div className="about">
+      <div className="presentation">
+        <motion.h2>PORTFOLIO</motion.h2>
+        <motion.h1>ÉTUDIANTE INGÉNIEURE EN INFORMATIQUE À POLYTECH LYON</motion.h1>
+        <motion.button
+          whileHover={{ scale: 1.02 }}
+          whileTap={{ scale: 0.98 }}
+          onClick={() => window.scrollTo({ top: window.innerHeight, behavior: 'smooth' })}
+        >
+          Découvrez mes projets
+        </motion.button>
       </div>
-      <div className='textContainer'>
-        <div className='title-section'>À propos de moi</div>
-
-        <div className='text-section'>
-          <p>Je m'appelle Clara, j'ai 20 ans et je suis actuellement étudiante ingénieure en informatique à Polytech Lyon.</p>
-          <p>Au cours de mon cursus, j'ai étudié les mathématiques, le développement d'applications, le développement web et la data science. J'ai également appris la gestion de projet, la communication et le travail en équipe.</p>
-          <p>J'aime construire des applications belles et efficaces en combinant mes connaissances techniques avec le design. Mon objectif est de produire des projets à la fois intuitifs et évolutifs.</p>
+      <div className='aboutContainer'>
+        <div className='avatarContainer'>
+          <img src="/avatar.png" alt="Avatar" />
         </div>
+        <div className='textContainer'>
+          <div className='title-section'>À propos de moi</div>
 
+          <div className='text-section'>
+            <p>Je m'appelle Clara, j'ai 20 ans et je suis actuellement étudiante ingénieure en informatique à Polytech Lyon.</p>
+            <p>Au cours de mon cursus, j'ai étudié les mathématiques, le développement d'applications, le développement web et la data science. J'ai également appris la gestion de projet, la communication et le travail en équipe.</p>
+            <p>J'aime construire des applications belles et efficaces en combinant mes connaissances techniques avec le design. Mon objectif est de produire des projets à la fois intuitifs et évolutifs.</p>
+          </div>
+        </div>
       </div>
-
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { motion} from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import "./hero.scss";
 import Lottie from "react-lottie";
 import animationData from "../../lotties/mouse-scroll.json";
@@ -31,16 +31,21 @@ const Hero = () => {
         }
     };
 
+    const { scrollYProgress } = useScroll();
+    const translateRight = useTransform(scrollYProgress, [0, 1], [0, 250]);
+    const translateLeft = useTransform(scrollYProgress, [0, 1], [0, -250]);
+    const translateRightFast = useTransform(scrollYProgress, [0, 1], [0, 1500]);
+
     return (
         <div className="hero">
             <div className="wrapper">
                 <motion.div className="textContainer" variants={textVariants} initial="initial" animate="animate">
-                    <motion.h2 variants={textVariants}>PORTFOLIO</motion.h2>
-                    <motion.h1 variants={textVariants} className="lastname">SITHIDEJ</motion.h1>
-                    <motion.h1 variants={textVariants} className="firstname">Clara</motion.h1>
+                    <motion.h2 style={{ translateY: translateLeft }}>PORTFOLIO</motion.h2>
+                    <motion.h1 style={{ translateY: translateLeft, translateX: translateLeft }} className="lastname">SITHIDEJ</motion.h1>
+                    <motion.h1 style={{ translateY: translateRight, translateX: translateRight }} className="firstname">Clara</motion.h1>
 
                     <div className="svgContainer">
-                        <motion.div className="mouseSvg" initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 2 }}>
+                        <motion.div style={{ translateY: translateRightFast }} initial={{ opacity: 0 }} whileInView={{ opacity: 1 }} transition={{ duration: 2 }}>
                             <Lottie options={defaultOptions} height={80} width={80} />
                         </motion.div>
                     </div>
